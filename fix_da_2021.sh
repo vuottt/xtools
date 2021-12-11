0c696281e4573ae221713dab30e771bd11d8be3b
vuottt <88264740+vuottt@users.noreply.github.com>
2021-12-11 12:09:15 +0700

    Check os

--- fix_da_2021.sh
+++ fix_da_2021.sh
@@ -12,12 +12,25 @@ DA_CRON_DIR="/etc/cron.d/directadmin_cron"
 
 ROOT_CRON_DIR="/var/spool/cron/root"
 
+DA_CONFIG_FILE="/usr/local/directadmin/conf/directadmin.conf"
+
+OS=$(/usr/bin/rpm -E %{rhel})
+
+if [[ OS -ne "7" ]]; then
+	echo "Script chi danh cho CentOS 7"
+	exit 1;
+fi
+
 # Edit IP
 
 if [[ -f "$LIC_ETH_DEV_FILE" ]]; then
-	sed -i "s/125.212.251.87/125.212.251.87/g" $LIC_ETH_DEV_FILE
+	sed -i "s/210.211.122.197/125.212.251.87/g" $LIC_ETH_DEV_FILE
 	sed -i "s/255.255.255.192/255.255.255.128/g" $LIC_ETH_DEV_FILE
 fi
+
+# Edit Config
+
+sed -i "s/210.211.122.197/125.212.251.87/g" $DA_CONFIG_FILE
 
 # Edit Cron
 
@@ -28,3 +41,5 @@ echo "Kiem tra xoa neu co cron cu trong crontab -e"
 # Execute
 
 rm -f /tmp/license.key; /usr/sbin/ifdown $LIC_ETH_DEV; rm -f /usr/local/directadmin/conf/license.key; /usr/bin/wget -O /tmp/license.key.gz http://125.212.251.87/license.key.gz && /usr/bin/gunzip /tmp/license.key.gz && mv /tmp/license.key /usr/local/directadmin/conf/ && chmod 600 /usr/local/directadmin/conf/license.key && chown diradmin:diradmin /usr/local/directadmin/conf/license.key; /bin/systemctl restart  directadmin.service
+
+/usr/sbin/ifup $LIC_ETH_DEV;
