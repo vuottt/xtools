322d504079cb9f0321ee27879db3d22d98141e3c
vuottt <88264740+vuottt@users.noreply.github.com>
2021-12-11 13:05:56 +0700

    Update fix_da_2021.sh

--- fix_da_2021.sh
+++ fix_da_2021.sh
@@ -2,11 +2,15 @@
 
 # Fix License DA VinaHost 2021 to 2022
 
-ETH_DEV=$(/usr/sbin/ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
+ETH_DEV=$(/usr/sbin/ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | grep -v "tun" | sed -e 's/^[[:space:]]*//')
 
 LIC_ETH_DEV="$ETH_DEV:100"
 
-LIC_ETH_DEV_FILE="/etc/sysconfig/network-scripts/$LIC_ETH_DEV"
+echo "Dev: $LIC_ETH_DEV"
+
+LIC_ETH_DEV_FILE="/etc/sysconfig/network-scripts/ifcfg-$LIC_ETH_DEV"
+
+echo "DevLic File: $LIC_ETH_DEV_FILE"
 
 DA_CRON_DIR="/etc/cron.d/directadmin_cron"
 
@@ -24,6 +28,7 @@ fi
 # Edit IP
 
 if [[ -f "$LIC_ETH_DEV_FILE" ]]; then
+	ip addr del 210.211.122.197/25 dev $LIC_ETH_DEV_FILE
 	sed -i "s/210.211.122.197/125.212.251.87/g" $LIC_ETH_DEV_FILE
 	sed -i "s/255.255.255.192/255.255.255.128/g" $LIC_ETH_DEV_FILE
 fi
@@ -34,12 +39,33 @@ sed -i "s/210.211.122.197/125.212.251.87/g" $DA_CONFIG_FILE
 
 # Edit Cron
 
+sed -i "/125.212.251.87/d" $DA_CRON_DIR
+sed -i "/210.211.122.197/d" $DA_CRON_DIR
+
 echo "0 12 */3 * * rm -f /tmp/license.key; /usr/sbin/ifdown $LIC_ETH_DEV; rm -f /usr/local/directadmin/conf/license.key; /usr/bin/wget -O /tmp/license.key.gz http://125.212.251.87/license.key.gz && /usr/bin/gunzip /tmp/license.key.gz && mv /tmp/license.key /usr/local/directadmin/conf/ && chmod 600 /usr/local/directadmin/conf/license.key && chown diradmin:diradmin /usr/local/directadmin/conf/license.key; /bin/systemctl restart  directadmin.service" >> $DA_CRON_DIR
 
 echo "Kiem tra xoa neu co cron cu trong crontab -e"
 
+# Fix service voi template cu
+
+cat <<EOF > /sbin/daifup 
+#!/bin/bash
+sleep 5
+/usr/sbin/ifconfig $LIC_ETH_DEV 125.212.251.87 netmask 255.255.255.128
+EOF
+
+cat <<EOF > /sbin/daifdown
+#!/bin/bash
+sleep 5
+/sbin/ip addr del 125.212.251.87/25  dev $LIC_ETH_DEV
+EOF
+
 # Execute
+
+sleep 1
 
 rm -f /tmp/license.key; /usr/sbin/ifdown $LIC_ETH_DEV; rm -f /usr/local/directadmin/conf/license.key; /usr/bin/wget -O /tmp/license.key.gz http://125.212.251.87/license.key.gz && /usr/bin/gunzip /tmp/license.key.gz && mv /tmp/license.key /usr/local/directadmin/conf/ && chmod 600 /usr/local/directadmin/conf/license.key && chown diradmin:diradmin /usr/local/directadmin/conf/license.key; /bin/systemctl restart  directadmin.service
 
-/usr/sbin/ifup $LIC_ETH_DEV;
+ifup $LIC_ETH_DEV
+
+service directadmin start
