5ea936203b3877ab50b2ba02c3519a09dd4c24b0
vuottt <88264740+vuottt@users.noreply.github.com>
2022-01-10 15:46:52 +0700

    Update install-speedtest-backend.sh

--- install-speedtest-backend.sh
+++ install-speedtest-backend.sh
@@ -15,22 +15,6 @@ else
         ROOTGRP=root
 fi
 
-WGET_OPTION="-T 10"
-if $WGET_PATH --help | grep -m1 -q connect-timeout; then
-        WGET_OPTION=" ${WGET_OPTION} --connect-timeout=10";
-fi
-COUNT=`$WGET_PATH --help | grep -c no-check-certificate`
-if [ "$COUNT" -ne 0 ]; then
-        WGET_OPTION="${WGET_OPTION} --no-check-certificate";
-fi
 
-HTTP=https
-EXTRA_VALUE=
-if [ "${INSECURE}" -eq 1 ]; then
-        HTTP=http
-        EXTRA_VALUE="&insecure=yes"
-fi
-
-
-${WGET_PATH} ${WGET_OPTION} -t 1 ${HTTP}://upload-vpn.vinahost.vn/d4kjBN/backend.tar.gz
+${WGET} --no-check-certificate https://upload-vpn.vinahost.vn/d4kjBN/backend.tar.gz
 ${TAR} xzf backend.tar.gz -c /var/www/html/
