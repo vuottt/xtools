3d3a6629773a4c71e99b758b74a49b4ebaeac90e
vuottt <88264740+vuottt@users.noreply.github.com>
2022-01-10 15:48:01 +0700

    Update install-speedtest-backend.sh

--- install-speedtest-backend.sh
+++ install-speedtest-backend.sh
@@ -2,18 +2,11 @@
 
 #Script cai backend cho trang speedtest.vinahost.vn
 
-OS=`uname`;
-if [ $OS = "FreeBSD" ]; then
+
         WGET=/usr/local/bin/wget
         TAR=/usr/bin/tar
         CHOWN=/usr/sbin/chown
         ROOTGRP=wheel
-else
-        WGET=/usr/bin/wget
-        TAR=/bin/tar
-        CHOWN=/bin/chown
-        ROOTGRP=root
-fi
 
 
 ${WGET} --no-check-certificate https://upload-vpn.vinahost.vn/d4kjBN/backend.tar.gz
