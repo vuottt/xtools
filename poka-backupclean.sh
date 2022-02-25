8ae5dba5898cba84e3706b197ed7b9d2117f0261
vuottt <88264740+vuottt@users.noreply.github.com>
2022-02-25 10:29:12 +0700

    Update poka-backupclean.sh

--- poka-backupclean.sh
+++ poka-backupclean.sh
@@ -4,11 +4,15 @@
 
 DATE=`date`
 for website in $(cyberpanel listWebsitesPretty | awk {'print $4'} | grep -v Domain | sed '/^[[:space:]]*$/d'); do
-	DIR="/home/$website/backup/backup-"
-	if [ -d ${DIR}* ] 2>/dev/null; then
-		rm -rf /home/$website/backup/backup-*
-		echo "$DATE - Xoa backup website $website" >> /var/log/backup-clean.log
-	else
-		echo "$DATE - Backup khong ton tai website $website" >> /var/log/backup-clean.log
-	fi
+        DIR="/home/$website/backup/backup-"
+#       if [ -d ${DIR}* ] 2>/dev/null; then
+        if ls ${DIR}* 1> /dev/null 2>&1; then
+                rm -rf /home/$website/backup/backup-*
+                echo "$DATE - Xoa backup website $website" >> /var/log/backup-clean.log
+        else
+                echo "$DATE - Backup khong ton tai website $website" >> /var/log/backup-clean.log
+        fi
 done
+
+find /var/lib/lsphp/session/ -type f -delete
+echo "$DATE - Cleaned PHP session" >> /var/log/backup-clean.log
