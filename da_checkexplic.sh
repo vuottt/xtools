dcd521641c921b318412d9c093d6798f7941cf23
vuottt <88264740+vuottt@users.noreply.github.com>
2022-03-04 12:05:41 +0700

    Update da_checkexplic.sh

--- da_checkexplic.sh
+++ da_checkexplic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MESSAGE_BODY="DirectAdmin license server Expired. Login vào server http://210.211.122.199:2222 kiểm tra ngay"
-EMAIL_SUBJECT="DirectAdmin license server Expired."
+MESSAGE_BODY="DirectAdmin license hết hạn. Login vào server http://210.211.122.199:2222 kiểm tra ngay"
+EMAIL_SUBJECT="DirectAdmin license Expired."
 FROM_EMAIL_ADDRESS="directadmin@vina-host.com"
 FRIENDLY_NAME="DirectAdmin CentOS7 License"
 SMTP_SERVER="e.vinahost.vn"
@@ -14,15 +14,6 @@ NSS_DIR="/etc/pki/nssdb/"
 
 if [[ $(/usr/bin/curl -s -u admin:zq0lSMu6TjMfWEo http://210.211.122.199:2222 | grep Expired) ]]; then
 	
-	/usr/local/directadmin/scripts/getLicense.sh jezuLqLxRFTz+Y9BtCEqPrLd760KRsker0dbVFqystI=
-	gzip < /usr/local/directadmin/conf/license.key > /var/www/html/license.key.gz
-	chmod 644 /var/www/html/license.key.gz
-	ln -sf /usr/share/zoneinfo/Asia/Saigon /etc/localtime
-	ntpdate asia.pool.ntp.org && hwclock -w > /dev/null
-	service directadmin restart
-	echo "$(date) : Da get lai license!" >> /var/log/da_license.log
-
-else
 	echo $MESSAGE_BODY | mailx -v -s "$EMAIL_SUBJECT" \
 	-S smtp-use-starttls \
 	-S ssl-verify=ignore \
@@ -34,6 +25,16 @@ else
 	-S ssl-verify=ignore \
 	-S nss-config-dir=$NSS_DIR \
 	$TO_EMAIL_ADDRESS
+	
+	/usr/local/directadmin/scripts/getLicense.sh jezuLqLxRFTz+Y9BtCEqPrLd760KRsker0dbVFqystI=
+	gzip < /usr/local/directadmin/conf/license.key > /var/www/html/license.key.gz
+	chmod 644 /var/www/html/license.key.gz
+	ln -sf /usr/share/zoneinfo/Asia/Saigon /etc/localtime
+	ntpdate asia.pool.ntp.org && hwclock -w > /dev/null
+	service directadmin restart
+	echo "$(date) : Da get lai license!" >> /var/log/da_license.log
+
+else
 	echo "LIcensed - Do nothing"
 
 fi
