9d892c981ba12c78e0e6ceb514a2aee713811d3e
vuottt <88264740+vuottt@users.noreply.github.com>
2022-03-07 10:07:27 +0700

    Update update_da.sh

--- update_da.sh
+++ update_da.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+DA_ETH_DEV=$(grep ethernet_dev /usr/local/directadmin/conf/directadmin.conf | cut -f2 -d"=")
+
 mv /usr/local/directadmin/update.tar.gz{,.v.1.61}
 if [[ $(hostnamectl | grep openvz) ]]; then
-/sbin/ip addr del 210.211.122.199/32  dev venet0:101
-/sbin/ip addr del 210.211.122.197/32  dev venet0:101
+# /sbin/ip addr del 210.211.122.199/32  dev venet0:101
+# /sbin/ip addr del 210.211.122.197/32  dev venet0:101
+if [[ $(ip addr show | grep 210.211.122.199 | awk {'print $7'}) ]]; then
+ETH_DEV=$(ip addr show | grep 210.211.122.199 | awk {'print $7'})
+else
+ETH_DEV=$(ip addr show | grep 210.211.122.197 | awk {'print $7'})
+fi
+/sbin/ip addr del 210.211.122.197/32  dev $ETH_DEV
+/sbin/ip addr del 210.211.122.199/32  dev $ETH_DEV
 sed -i 's/210.211.122.197/210.211.122.199/g' /sbin/daifdown
 sed -i 's/210.211.122.197/210.211.122.199/g' /sbin/daifup
+sed -i "s/$ETH_DEV/$DA_ETH_DEV/g" /sbin/daifdown
+sed -i "s/$ETH_DEV/$DA_ETH_DEV/g" /sbin/daifup
 else
 ifdown eth0:100
 ifdown eht0:101
